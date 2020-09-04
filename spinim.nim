4dc8030d24981d70827987692f572d58cd46c1cd
py-am-i <Wykleph@users.noreply.github.com>
2020-09-04 15:11:35 -0700

    Fixed bug with py export pragma. removed unused imports.

--- spinim.nim
+++ spinim.nim
@@ -1,27 +1,20 @@
 import re
-import unicode
 import random
 import nimpy
-import sequtils
 import strutils
 randomize()
 
-
-let spintax_separator: Regex = re(r"((?:(?<!\\)(?:\\\\)*))(\|)")
 let spintax_bracket: Regex = re(r"(?<!\\)((?:\\{2})*)\{([^}{}]+)(?<!\\)((?:\\{2})*)\}")
-
 
 proc random_pick(text: string, full_text: string): string =
     let replacement = text[1 .. text.high - 1].split("|").sample()    
     return full_text.replace(text, replacement)
 
-
-proc spin(text: string): {.exportpy.} string = 
+proc spin(text: string): string {.exportpy.} = 
     var new_string: string = text
     while true:
         var matches: seq[string] = findAll(new_string, spintax_bracket)
-        if matches == @[]:
-            break
+        if matches == @[]: break
         for m in matches:
             new_string = random_pick(m, new_string)
     return new_string
