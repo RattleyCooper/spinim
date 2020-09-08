73db44c206b367e805153741c9dd968b83164653
py-am-i <Wykleph@users.noreply.github.com>
2020-09-08 10:28:58 -0700

    Use `result` instead of `return`

--- spinim.nim
+++ spinim.nim
@@ -1,21 +1,21 @@
 import re
 import random
-import nimpy
 import strutils
 randomize()
+import nimpy
+
 
 let spintax_bracket: Regex = re(r"(?<!\\)((?:\\{2})*)\{([^}{}]+)(?<!\\)((?:\\{2})*)\}")
 
 proc random_pick(text: string, full_text: string): string =
     let replacement = text[1 .. text.high - 1].split("|").sample()    
-    return full_text.replace(text, replacement)
+    result = full_text.replace(text, replacement)
 
 proc spin(text: string): string {.exportpy.} = 
     var new_string: string = text
     while true:
-        var matches: seq[string] = findAll(new_string, spintax_bracket)
+        let matches: seq[string] = findAll(new_string, spintax_bracket)
         if matches == @[]: break
         for m in matches:
             new_string = random_pick(m, new_string)
-    return new_string
-
+    result = new_string
