d6c63bb88c63a4ae32e61337b01644167bb4dabe
py-am-i <Wykleph@users.noreply.github.com>
2021-07-21 10:59:29 -0700

    Prep for nimble release

--- src/spinim.nim
+++ src/spinim.nim
@@ -6,16 +6,17 @@ import nimpy
 
 
 let spintax_bracket: Regex = re(r"(?<!\\)((?:\\{2})*)\{([^}{}]+)(?<!\\)((?:\\{2})*)\}")
+let empty_seq: seq[string] = @[]
 
 proc random_pick(text: string, full_text: string): string =
     let replacement = text[1 .. text.high - 1].split("|").sample()    
     result = full_text.replace(text, replacement)
 
-proc spin(text: string): string {.exportpy.} = 
+proc spin*(text: string): string {.exportpy.} = 
     var new_string: string = text
     while true:
         let matches: seq[string] = findAll(new_string, spintax_bracket)
-        if matches == @[]: break
+        if matches == empty_seq: break
         for m in matches:
             new_string = random_pick(m, new_string)
     result = new_string
